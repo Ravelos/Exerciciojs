77d5a62f6dce0c96f60090b6f1aae123046e499f
Ravelos <86490406+Ravelos@users.noreply.github.com>
2021-09-24 11:02:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let nome = prompt('digite seu nome')
+let sobrenome = prompt('Digite seu sobrenome')
+let idade = prompt('Qual é a sua idade')
+
+console.log('nome:' +nome);
+console.log('sobrenome:' + sobrenome)
+console.log('idade:' + idade)
